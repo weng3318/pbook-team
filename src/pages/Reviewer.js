82db0720f177ae4be5ad37db148f78f4f2c33a9e
weng3318 <weng7395@gmail.com>
2019-11-01 08:31:12 +0800

    1101add

--- src/pages/Reviewer.js
+++ src/pages/Reviewer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import BR_ReviewerList from './reviewer_page/BR_ReviewerList'
 import BR_TodoWith from './reviewer_page/BR_TodoWith'
 import BR_DateTime from './reviewer_page/BR_DateTime'
--- src/pages/reviewer_page/BR_ReviewerList.js
+++ src/pages/reviewer_page/BR_ReviewerList.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import '../reviewer_page/BR_ReviewerList.css'
 
 export class BR_ReviewerList extends React.Component {
   render() {
     return (
       <>
-        <div className="d-flex">
+        <section className="d-flex">
           <div className="brAvatar borderLine">頭像
           <img className="brAvatarImg" src={require('../reviewer_page/images/31桑.png')}/>
-          <h5>書評家</h5>
+          <h5>書評家<p>小說家、編劇、總編輯</p></h5>
+          
           </div>
 
           <div className="brInfo borderLine">自我介紹
@@ -27,8 +29,14 @@ export class BR_ReviewerList extends React.Component {
 
           <div className="brDesk borderLine">我的書櫃
             <img className="brDeskBook" src={require('../reviewer_page/images/01書_說話之道.png')}/>
-            <iframe className="brYouTube" width="245" height="140" src="https://www.youtube.com/embed/1Thg-O_RPe4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-          </div>
+            {/* <iframe className="brYouTube" width="245" height="140" src="https://www.youtube.com/embed/1Thg-O_RPe4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+            <iframe className="brYouTube" width="245" height="140" src="https://www.youtube.com/embed/uVIpHQMzvR8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div className="toFB">
+            123
+            </div>
+            {/* <Link to="https://youtu.be/6_fzj0-VRHM">link</Link> */}
+            </div>
+        </section>
       </>
     )
   }
