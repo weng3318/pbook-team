c09df931671b3c317066bd08bf6d85afa049e5a7
weng3318 <weng7395@gmail.com>
2019-11-02 15:56:14 +0800

    1102add

--- src/pages/reviewer_page/BR_ReviewerList.js
+++ src/pages/reviewer_page/BR_ReviewerList.js
@@ -8,17 +8,18 @@ export class BR_ReviewerList extends React.Component {
   render() {
     return (
       <>   
-        <section className="ReviewerListAllBox">
+<section className="ReviewerListAllBox">
     <div className="d-flex">
 
-          <div className=".brAvatarBox borderLine">頭像
+          <div className="brAvatarBox borderLine">
               <img className="brAvatarImg" src={require('../reviewer_page/images/31桑.png')}/>
               <h5>書評家<p>小說家、編劇、總編輯</p></h5>
           </div>
 
-          <div className=".brAvatarBox borderLine">頭像
-              <h5>書評家簡介
-               <p>自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 
+          <div className="brInfoBox borderLine"><h5>書評家簡介</h5>
+              <div className="brInfoText borderLine">
+              <p>
+                  自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 
                   自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 
                   自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
                   自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
@@ -28,55 +29,37 @@ export class BR_ReviewerList extends React.Component {
                   自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
                   自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
                 </p>
-              </h5>
-            </div>
-          {/* <div className="brInfoBox borderLine">自我介紹
-              <div className="brInfoText borderLine">
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
-                自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 > 自我介紹 >
               </div>
-          </div> */}
+          </div>
     </div>
 
-          <div className="brMediaBox borderLine">我的書櫃
 
-          <div className="d-flex">
-          {/* 超連結 → 我的書櫃 */}
-            <div className="brMyBooks borderLine">
-              <Link to="/reviewerBooks">
-                <img className="brDeskBook borderLine" src={require('../reviewer_page/images/01書_說話之道.png')}/>
-                <p className="textReadBook">看看書櫃</p>
-              </Link>
-            </div>
-          
-            <div className="brMyVideo borderLine">
-                <iframe className="brYouTube" width="245" height="140" src="https://www.youtube.com/embed/uVIpHQMzvR8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <p className="textReadVideo">書評影片</p>
-            </div>
-          </div>
+    <div className="brMyBooksBox borderLine">
+          {/* <img className="brBookSize borderLine" src={require('../reviewer_page/images/01書_說話之道.png')}/> */}
+          <p className="textMedia">我的書櫃</p>
+      <Link to="/reviewerBooks">
+          <img className="brMyBooks borderLine"/>
+      </Link>
+      </div>
+      
+        <div className="brMyVideoBox borderLine">
+            <p className="textMedia">書評影片</p>
+            <Link to="/reviewerBooks">
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/uVIpHQMzvR8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Link>
+        </div>
 
-            <section className="brIconBox d-flex borderLine">
-            <div className="brIconCommunity d-flex borderLine">
-                <img className="brIcon" src={require('../reviewer_page/images/icon_youtube.png')}/>
-                <img className="brIcon" src={require('../reviewer_page/images/icon_facebook.png')}/>
-                <img className="brIcon" src={require('../reviewer_page/images/icon_twitter.png')}/>
-            </div>
 
-            <div className="brIconFollow d-flex borderLine">
-                <img className="brIcon" src={require('../reviewer_page/images/icon_follow.png')}/>
-            </div>
-            </section>
 
-          </div>
-        
-          </section>
+        {/* <div className="brIconFollow d-flex borderLine">
+              <img className="brIcon" src={require('../reviewer_page/images/icon_youtube.png')}/>
+              <img className="brIcon" src={require('../reviewer_page/images/icon_facebook.png')}/>
+              <img className="brIcon" src={require('../reviewer_page/images/icon_twitter.png')}/>
+              <img className="brIcon" src={require('../reviewer_page/images/icon_follow.png')}/>
+        </div> */}
+
+
+</section>
         {/* <Router>
          <Switch>
           <Route exact path="/reviewerBooks" component={ReviewerBooks} />     
